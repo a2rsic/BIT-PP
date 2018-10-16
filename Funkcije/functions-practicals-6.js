0e13119e696a82b899e3fe30b8c2d2ced8f548c5
BIT student <classroom@bgit.rs>
2018-10-16 17:54:31 +0200

    funkcije

--- Funkcije/functions-practicals-6.js
+++ Funkcije/functions-practicals-6.js
@@ -1,5 +1,5 @@
 // Write a function to count vowels in a provided string. If you are  not aware of indexOf function, try to use switch statement.
-
+"use strict"
 function vowels(string) {
   var count = 0;
   for (var i = 0; i < string.length; i++) {
@@ -30,7 +30,7 @@ console.log(vowels("ana voli milovana"));
 
 // Modify
 
-var vowels = function(string) {
+var vowels = function (string) {
   var count = 0;
   for (var i = 0; i < string.length; i++) {
     switch (string[i]) {
@@ -61,7 +61,7 @@ console.log(vowels("ana voli milovana"));
 // Write a function that combines two arrays by alternatingly taking elements.
 // [‘a’,’b’,’c’], [1,2,3] -> [‘a’,1,’b’,2,’c’,3]
 
-var combinesTwoArray = function(arr1, arr2) {
+var combinesTwoArray = function (arr1, arr2) {
   var newArr = [];
   for (var i = 0; i < arr1.length && i < arr2.length; i++) {
     newArr[newArr.length] = arr1[i];
@@ -75,7 +75,7 @@ console.log(combinesTwoArray(["a", "b", "c"], [1, 2, 3]));
 // Write a function that rotates a list by k elements.
 // For example [1,2,3,4,5,6] rotated by two becomes [3,4,5,6,1,2]
 
-var rotatesList = function(array, num) {
+var rotatesList = function (array, num) {
   var newArr = [];
   var counter = 0;
   for (var i = num; i < array.length; i++) {
@@ -92,7 +92,7 @@ console.log(rotatesList([1, 2, 3, 4, 5, 6], 3));
 
 // Write a function that takes a number and returns array of its digits.
 
-var returnsArray = function(number) {
+var returnsArray = function (number) {
   var numberString = number + "";
   var arrayNew = [];
   var counter = 0;
@@ -106,7 +106,7 @@ console.log(returnsArray(1254));
 
 // Write a program that prints a multiplication table for numbers up to 12.
 
-var multiplicationTable = function(num) {
+var multiplicationTable = function (num) {
   var table = "";
 
   for (var i = 1; i <= num; i++) {
@@ -122,7 +122,7 @@ console.log(multiplicationTable(10));
 
 // Write a function to input temperature in Centigrade and convert to Fahrenheit.
 
-var convertToF = function(celsius) {
+var convertToF = function (celsius) {
   return (celsius * 9) / 5 + 32;
 };
 
@@ -130,7 +130,7 @@ console.log(convertToF(20));
 
 // Write a function to find the maximum element in array of numbers. Filter out all non-number elements.
 
-var findMaxNum = function(arr) {
+var findMaxNum = function (arr) {
   var newArr = [];
 
   var i;
@@ -153,7 +153,7 @@ console.log(findMaxNum(["1", "21", undefined, "42", "1e+3", Infinity]));
 
 // Write a function to find the maximum and minimum elements. Function returns an array.
 
-var findMaxMin = function(array) {
+var findMaxMin = function (array) {
   var newArr = [];
   var max = array[0];
   var min = array[0];
@@ -191,7 +191,7 @@ console.log(findMaxMin([1, 2, 4, 50, 10, 29]));
 // Obese: greater than or equal to 30 but less than 40
 // Morbidly obese: greater than or equal to 40
 
-var bodyMassIndex = function(weight, height) {
+var bodyMassIndex = function (weight, height) {
   var bmi = weight / (height * height);
 
   var result = "";
@@ -215,7 +215,7 @@ var bodyMassIndex = function(weight, height) {
   return result;
 };
 
-console.log(bodyMassIndex(80, 1.8));
+console.log(bodyMassIndex(85, 1.75));
 
 // Write a function that takes a list of strings and prints them, one per line, in a rectangular frame.:
 // For example the list ["Hello", "World", "in", "a", "frame"] gets printed as:
